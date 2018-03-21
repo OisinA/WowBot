670e0d165381ea012b7a02208411979d1b190749
OisinA <oisinaylward@gmail.com>
2018-03-21 23:32:27 +0000

    Added the lyric command.

---
+++ lyricCommand.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	discord "github.com/bwmarrin/discordgo"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func LyricCommand(s *discord.Session, m *discord.MessageCreate, message string) {
+	if m.Author.Bot {
+		return
+	}
+	split := strings.Split(message, "-")
+	ch := make(chan string)
+	go GetLyrics(ch, split[0], split[1])
+	lyrics := <-ch
+	lyricList := make([]string, len(lyrics)/1000)
+	length := 0
+	for {
+		if length+1000 > len(lyrics) {
+			lyricList = append(lyricList, lyrics[length-1000:])
+			break
+		}
+		lyricList = append(lyricList, lyrics[length:(length+1000)])
+		length += 1000
+	}
+	s.ChannelMessageSend(m.ChannelID, "Lyrics: ")
+	for _, i := range lyricList {
+		s.ChannelMessageSend(m.ChannelID, string(i))
+	}
+}
+
+func GetLyrics(ch chan string, artist string, song string) {
+	resp, _ := http.Get("https://api.lyrics.ovh/v1/" + artist + "/" + song)
+	read, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- ""
+		return
+	}
+	var dat map[string]interface{}
+	json.Unmarshal([]byte(read), &dat)
+	ch <- dat["lyrics"].(string)
+}
