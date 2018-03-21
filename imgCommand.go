4388d8b0140f7690a5c7a2983d7aa20a0db63791
OisinA <oisinaylward@gmail.com>
2018-03-21 22:49:57 +0000

    img command now shows NASA's photo of the day.

--- imgCommand.go
+++ imgCommand.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"fmt"
 	discord "github.com/bwmarrin/discordgo"
+	"golang.org/x/net/html"
+	"net/http"
 )
 
 func ImgCommand(s *discord.Session, m *discord.MessageCreate, message string) {
 	if m.Author.Bot {
 		return
 	}
+	img := GetImage()
+	fmt.Println(img)
 	s.ChannelMessageSendEmbed(m.ChannelID, &discord.MessageEmbed{
 		Title:       "Image",
 		Description: message,
 		Image: &discord.MessageEmbedImage{
-			URL: "https://picsum.photos/200/300/?random",
+			URL: GetImage(),
 		},
 	})
+}
+
+func GetImage() string {
+	resp, _ := http.Get("https://apod.nasa.gov/apod/astropix.html")
+	z := html.NewTokenizer(resp.Body)
+OUTERLOOP:
+	for {
+		iter := z.Next()
+		switch {
+		case iter == html.ErrorToken:
+			break OUTERLOOP
+		case iter == html.StartTagToken:
+			t := z.Token()
+
+			isImage := t.Data == "img"
+			if isImage {
+				for _, a := range t.Attr {
+					if a.Key == "src" {
+						return "https://apod.nasa.gov/apod/" + a.Val
+					}
+				}
+			}
+		}
+	}
+	return ""
 }
